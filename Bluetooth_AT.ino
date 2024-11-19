eeb55c0ba4c32774d785d871eda8a3f3ce074a30
ykbl27 <135182453+ykbl27@users.noreply.github.com>
2024-11-18 16:29:57 -0800

    Update 2 Bluetooth_AT.ino
    removed delays: will speed up program and prevent potential sequential programming errors

--- Bluetooth_AT.ino
+++ Bluetooth_AT.ino
@@ -1,46 +1,67 @@
-  #include "SoftwareSerial.h"
+#include "SoftwareSerial.h"
 // Using software serial port, can speak digital port analog into serial port
 SoftwareSerial BT(2, 3);  //New object, receive foot 2, send foot 3
-char val;  //Store received variables
-String cmd = "";
- 
+char cmd[64] = {0};
+int cmdIndex = 0;
+bool cmdSent = false;
+
+char cmd2[64] = {0};
+int cmd2Index = 0;
+
 void setup() {
+  pinMode(2, INPUT);  //rxPin
+  pinMode(3, OUTPUT); //txPin
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.begin(9600);   //Serial Port Connection with Computer
   Serial.println("BT Module is ready!");
   BT.begin(9600);  //set baud rate
   BT.write("AT"); //Send AT.
-  cmd = "";
 }
- 
+
 void loop() {
-  //Bluetooth to Serial, write all commands here to Serial
+  
+  //PHONE TO BT on Arduino
+  //read input
+  while (BT.available()) {
+    char c = BT.read();
+    if (c == ';') {
+      cmd[cmdIndex] = '\0';  // Null-terminate the string
+      cmdIndex = 0;  // Reset buffer for next message     
+      cmdSent = true;
+    } else if (cmdIndex < sizeof(cmd) - 1) {
+      cmd[cmdIndex++] = c;  // Add character to buffer
+    }
+  }
+
+  //// COMMANDS ////    // when sending from phone end command with ';' ex: "on;" "off;"
+  if (cmdSent) {
+    cmdSent = false;
+    Serial.write(cmd);  
+    if (strncmp(cmd, "on", strlen("on")) == 0) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      Serial.println(": COMMAND SENT FROM PHONE: TURN ON");
+    }
+    else if (strncmp(cmd, "off", strlen("off")) == 0) {
+      digitalWrite(LED_BUILTIN, LOW);
+      Serial.println(": COMMAND SENT FROM PHONE: TURN OFF");
+    }
+    else {  // if sent message doesn't fit commands
+      Serial.println(": INVALID COMMAND");
+    }  
+  }
+
   while (Serial.available()) {
-    if (cmd == "") {
-      delay(20);
+    char c2 = Serial.read();
+    if (c2 == '\n') {  // Detect newline (message terminator)
+      cmd2[cmd2Index] = '\0';  // Null-terminate the string
+      Serial.print(cmd2);
+      Serial.println(" : PHONE RECEIVING");
+      BT.write(cmd2);  //send data back to PHONE      
+      cmd2Index = 0;  // Reset buffer for next message
+    } else if (cmd2Index < sizeof(cmd2) - 1) {
+      cmd2[cmd2Index++] = c2;  // Add character to buffer
     }
-    val = Serial.read();
-    cmd += val;
   }
-  if (cmd != "") {
-    BT.write(cmd.c_str());
-    Serial.write(cmd.c_str());
-    if (cmd =="on"){
-     digitalWrite(LED_BUILTIN, HIGH);
-    }
-    else if (cmd =="off"){
-     digitalWrite(LED_BUILTIN, LOW); 
-    }       
-    Serial.println(": SERIAL SENT");
-    cmd = "";
-  }
- 
-  //Serial to Bluetooth, send commands to BT
-  if (BT.available()) {
-    Serial.println("BT SENT");
-    val = BT.read();
-    Serial.write(val);
-    BT.write(val);    
-  }
+
 }
