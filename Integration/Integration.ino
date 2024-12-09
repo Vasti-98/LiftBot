081ca895e973fc8c6edb64e9dfe5b90459d482a7
Cheyenne <cheyenne.mt.arnold@gmail.com>
2024-12-09 12:22:11 -0800

    Started integration framework

--- Integration/Integration.ino
+++ Integration/Integration.ino
@@ -1,8 +1,53 @@
 
 
 // Timing variables
+#include <stdio.h>
 unsigned long previousTime = 0;
-const unsigned long samplingInterval = 100; // Milliseconds for color sensor sampling
+
+#define IDLE_STATE 0;
+#define DRIVING_STATE 1;
+#define PICKUP_STATE 2;
+#define T90_LEFT_STATE 3;
+#define T90_RIGHT_STATE 4;
+#define DROPPING_STATE 5;
+#define T180_STATE 6;
+
+#define BT_START_EVENT 7;
+#define BT_STOP_EVENT 8;
+#define LLINE_DETECTED_EVENT 9;
+#define RLINE_DETECTED_EVENT 10;
+#define RED_DETECTED_EVENT 11;
+#define GREEN_DETECTED_EVENT 12;
+#define BLUE_DETECTED_EVENT 13;
+
+#define UNSORTED_BIN 14;
+#define RED_BIN 15;
+#define GREEN_BIN 16;
+#define BLUE_BIN 17;
+
+
+volatile int currentTarget = UNSORTED_BIN;
+volatile bool returning = true;
+
+
+
+int handleStateTransition(int currentState, int event) {
+  switch(currentState) {
+    case IDLE_STATE:
+      if (event == BT_START_EVENT) {
+        currentTarget = UNSORTED_BIN;
+        return DRIVING_STATE;
+      }
+      break;
+    case DRIVING_STATE:
+      if (event == LLINE_DETECTED_EVENT) {
+        if ()
+      }
+      
+  }
+}
+
+
 
 void setup() {
 
@@ -14,10 +59,5 @@ int readColorSensor() {
 
 
 void loop() {
-  if (btOn == true) {
-    
-  } else if (btOn == false){
-    
-  } 
 
 }
